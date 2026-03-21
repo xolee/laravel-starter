6eb7c91cc649a47c55d7d9a7564db5be6fc0c8c9
xolee <xolee@qq.com>
2026-03-21 17:52:36 +0800

    feat(filament): 新增 AppPanelProvider 和 CentralPanelProvider 面板
    - 创建并配置 AppPanelProvider 以管理应用端面板
    - 创建并配置 CentralPanelProvider 以管理中央控制台面板

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AppPanelProvider::class,
+    App\Providers\Filament\CentralPanelProvider::class,
 ];
